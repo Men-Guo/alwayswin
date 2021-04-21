6e56b70bc646b01d45655b214aeab84abeb7407c
Zihao <zwang22@ucla.edu>
2021-04-21 05:59:13 -0700

    -m

--- src/main/java/com/example/alwayswin/controller/WishListController.java
+++ src/main/java/com/example/alwayswin/controller/WishListController.java
@@ -5,6 +5,8 @@ import com.example.alwayswin.entity.WishList;
 import com.example.alwayswin.service.WishListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,18 +17,18 @@ public class WishListController {
     @Autowired
     private WishListService wishListService;
 
-    @RequestMapping(value = "/uid")
+    @RequestMapping(value = "/uid", method = RequestMethod.GET)
     public List<WishList> getByUid(Integer uid){
         List<WishList> wishLists = wishListService.queryWishList(uid);
         return wishLists;
     }
 
-    @RequestMapping(value ="/wid")
+    @RequestMapping(value ="/wid", method = RequestMethod.GET)
     public WishList getByWid(Integer wid){
         return wishListService.queryWishListByWid(wid);
     }
 
-    @RequestMapping(value ="/delete")
+    @RequestMapping(value ="/delete", method = RequestMethod.GET)
     public int deleteWishList(Integer uid,Integer pid){
         int res = wishListService.deleteWishList(uid,pid);
         if (res == 1){
@@ -37,5 +39,15 @@ public class WishListController {
         return res;
     }
 
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public int insertWishList(WishList wishList){
+        int res = wishListService.addWishList(wishList);
+        if (res == 1){
+            System.out.println("操作成功");
+        }else{
+            System.out.println("操作失败");
+        }
+        return res;
+    }
 
 }
