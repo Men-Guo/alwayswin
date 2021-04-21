cb8cbed61eb9e7536d4c4098cd4e9f004890ec68
Zihao <zwang22@ucla.edu>
2021-04-21 03:52:00 -0700

    -m

---
+++ src/test/java/com/alwayswin/AlwaysWinVersion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alwayswin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlwaysWinVersion1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
