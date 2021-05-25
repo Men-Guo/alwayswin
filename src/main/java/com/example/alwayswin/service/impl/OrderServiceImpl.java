6c4bad0f195aca3f737e001840b5ecc326b1beb4
Zihao <zwang22@ucla.edu>
2021-05-24 20:35:56 -0700

    添加timer

--- src/main/java/com/example/alwayswin/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/alwayswin/service/impl/OrderServiceImpl.java
@@ -77,9 +77,10 @@ public class OrderServiceImpl implements OrderService {
             Order oldOrder = orderMapper.getByOid(oid);
 
             // 操作者是买家
-            if (uid == order.getUid()) {
+            if (uid == oldOrder.getUid()) {
                 // placed -> paid, 同时进行的还有改地址
                 if (oldOrder.getStatus().equals(OrderStatusCode.PLACED.getStatus())) {
+
                     if (order.getStatus().equals(OrderStatusCode.PAID.getStatus())) {
                         if (payOrder(order) == -1) { // 钱没够，先充钱
                             return -4;
