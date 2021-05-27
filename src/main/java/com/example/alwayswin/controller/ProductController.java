46b5edbf7525e249b865dca464e11beafcd15754
Zihao <zwang22@ucla.edu>
2021-05-27 00:28:02 -0700

    fix bug

--- src/main/java/com/example/alwayswin/controller/ProductController.java
+++ src/main/java/com/example/alwayswin/controller/ProductController.java
@@ -135,6 +135,7 @@ public class ProductController {
                                                               @RequestParam(value = "cate",required = false) String cate,
                                                               @RequestParam(value = "page",required = false, defaultValue = "1") Integer page,
                                                               @RequestParam(value = "pageSize",required = false,defaultValue = "5") Integer pageSize){
+
         List<ProductPreview> productPreviewList;
         PageHelper.startPage(page, pageSize);
         if (sortedBy == null || ordering == null) {
